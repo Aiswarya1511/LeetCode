e4487481442c5ac64fb99026d98d666dcfbfa3b8
AISWARYA R <aiswarya06211@gmail.com>
2026-08-21 21:46:20 +0530

    Time: 1 ms (99.78%) | Memory: 47.2 MB (76.60%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,13 +1,26 @@
 class Solution {
     public int jump(int[] nums) {
-      int jumps = 0, currEnd = 0, currFarthest = 0;
-      for(int i = 0; i < nums.length - 1; i ++) {
-          currFarthest = Math.max(currFarthest, i + nums[i]);
-          if(i == currEnd) {
-              currEnd = currFarthest;
-              jumps ++;
-          }
-      }  
-      return jumps;
+        int n = nums.length;
+        if (n <= 1) return 0;
+
+        int jumps = 0;
+        int currEnd = 0;
+        int maxReach = 0;
+
+        // Process indices until second last.
+        for (int i = 0; i < n - 1; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+
+            // End of current range => commit one jump.
+            if (i == currEnd) {
+                jumps++;
+                currEnd = maxReach;
+
+                // If this range already covers last index, stop.
+                if (currEnd >= n - 1) break;
+            }
+        }
+
+        return jumps;
     }
 }
